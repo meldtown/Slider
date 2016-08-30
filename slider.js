9c345b49523b49ddd56955b6479a324892ccdf77
RABOTA\VitaliyM <meldtown@gmail.com>
2016-08-30 13:00:01 +0300

    autotoggler is enabled to update bottom toggler

--- slider.js
+++ slider.js
@@ -9,30 +9,40 @@ var currentSlide = 2,
  	slideWrapper = $('.slide-wrapper'),
  	slideWrapperWidth = slideWrapper.width(totalSlideCount * 100 / slideCount + '%'),
  	slideElemsWidth,
+	autoToggleId,
 	slideToggler = $('.slide-toggler'),
  	leftButton = $('.slide-left'),
  	rightButton = $('.slide-right');
 
- 	// slideWrapper.css('marginLeft', slideWidthPercentFormatted + '%');
+ 	// slideWrapper.css('marginLeft', slideWidthPercentFormatted + '%'); - Initial shift
  	slideElems.width((100 / totalSlideCount ) + '%');
  	slideElemWidth = $(slideElems[0]).width();
 
 
- 	function relocateSlideToRight() {
- 		$(slideElems[0]).appendTo(slideWrapper);
- 		slideElems = $('.slide');
- 	}
+ 	// function relocateSlideToRight() {
+ 	// 	$(slideElems[0]).appendTo(slideWrapper);
+ 	// 	slideElems = $('.slide');
+ 	// }
+     //
+ 	// function relocateSlideToLeft() {
+ 	// 	$(slideElems[totalSlideCount - 1]).prependTo(slideWrapper);
+ 	// 	slideElems = $('.slide');
+ 	// }
 
- 	function relocateSlideToLeft() {
- 		$(slideElems[totalSlideCount - 1]).prependTo(slideWrapper);
- 		slideElems = $('.slide');
- 	}
-
+     //=====================================Arrow togglers========================================
  	function getCurrentToggler(index) {
  		return [].filter.call(slideToggler, function (item) {
 			return $(item).data('toggler-index') === index;
 		}).shift();
 	};
+
+	function leftSlideLogic() {
+		slideWrapper.stop(false, true).animate({'marginLeft': '-=' + slideWidthPercentFormatted + '%'});
+	}
+
+	function rightSlideLogic() {
+		slideWrapper.stop(false, true).animate({'marginLeft': '+=' + slideWidthPercentFormatted + '%'});
+	}
 
  	function slideToLeft() {
  		if (currentSlide === totalSlideCount) return;
@@ -40,7 +50,7 @@ var currentSlide = 2,
 		var currentToggler = getCurrentToggler(currentSlide);
 		slideToggler.removeClass('active-toggler');
 		$(currentToggler).addClass('active-toggler');
- 		slideWrapper.stop(false, true).animate({'marginLeft': '-=' + slideWidthPercentFormatted + '%'});
+		leftSlideLogic();
  	}
 
  	function slideToRight() {
@@ -49,12 +59,15 @@ var currentSlide = 2,
 		var currentToggler = getCurrentToggler(currentSlide);
 		slideToggler.removeClass('active-toggler');
 		$(currentToggler).addClass('active-toggler');
- 		slideWrapper.stop(false, true).animate({'marginLeft': '+=' + slideWidthPercentFormatted + '%'});
+		rightSlideLogic();
  	}
 
  	leftButton.click(slideToLeft);
  	rightButton.click(slideToRight);
 
+    //=========================================================================
+
+    //=================================== Bottom togglers =====================
 	slideToggler.click(function () {
 		var index = $(this).data('toggler-index'),
 			diff = Math.abs(index - currentSlide);
@@ -65,6 +78,43 @@ var currentSlide = 2,
 		slideToggler.removeClass('active-toggler');
 		$(this).addClass('active-toggler');
 	});
+
+//=========================================================================
+
+//============================== Autotoggler ==============================
+
+function relocateSlide() {
+    $(slideElems[0]).appendTo(slideWrapper);
+    slideWrapper.css('marginLeft', '0');
+    if (currentSlide === totalSlideCount) {
+        currentSlide = 1;
+    } else {
+        currentSlide++;
+    }
+    var currentToggler = getCurrentToggler(currentSlide);
+    slideToggler.removeClass('active-toggler');
+    $(currentToggler).addClass('active-toggler');
+}
+
+function Autotoggle() {
+	autoToggleId = setInterval(function () {
+		leftSlideLogic();
+        setTimeout(relocateSlide, 500);
+        slideElems = $('.slide');
+        console.log(currentSlide);
+	}, 3400);
+}
+Autotoggle();
+
+slider.hover(function () {
+	clearInterval(autoToggleId)
+}, function () {
+	Autotoggle()
+});
+
+
+
+
 
 
 
